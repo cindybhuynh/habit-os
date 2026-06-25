71d98716937fc2fe043c9cff901c80969821df4d
cindybhuynh <chbmv@umsystem.edu>
2026-06-24 23:39:26 -0500

    feat: add user auth schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,1 +1,20 @@
 # app/schemas/user.py
+from datetime import datetime
+
+from pydantic import BaseModel, Field, ConfigDict, EmailStr
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=8)
+    
+class UserRead(BaseModel): 
+    model_config = ConfigDict(from_attributes=True) # connects schema to ORM models
+
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
