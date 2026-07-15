55a51d135521618d3afdfad170c9d8fffa7dcacb
cindybhuynh <chbmv@umsystem.edu>
2026-07-14 23:00:46 -0500

    feat: add animated wave to login page and resize login container

--- habitos-frontend/src/LoginPage.jsx
+++ habitos-frontend/src/LoginPage.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import './App.css'
+import Wave from 'react-wavify'
 
 const API_URL = import.meta.env.VITE_API_URL
 
@@ -39,17 +40,44 @@ function LoginPage({onLogin, onSwitchToRegister}) {
   }
   
   return (
-    <div className="login-container">
-      <h1>HabitOS</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button type="submit">Log In</button>
-        <p className="switch-to-register-button" onClick={onSwitchToRegister}>
-          Don't have an account? Sign up
-        </p>
-      </form>
-      {error && <p className="error-message">{error}</p>}
+    <div className='login-page-wrapper'>
+      <div className="login-container">
+        <h1>HabitOS</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button type="submit">Log In</button>
+          <p className="switch-to-register-button" onClick={onSwitchToRegister}>
+            Don't have an account? Sign up
+          </p>
+        </form>
+        {error && <p className="error-message">{error}</p>}
+      </div>
+      <Wave
+      //  fill='#15A0D8'
+       paused={false}
+       options={{height: 45, amplitude: 20, speed: 0.13, points: 3 }}
+       style={{
+        position: 'absolute',
+        bottom: -7,
+        left: 0,
+        right: 0,
+        display: 'block',
+        opacity: 0.8,
+        marginBottom: 0,
+       }}
+       mask="url(#mask)" 
+       fill="#15A0D8">
+        <defs>
+          <linearGradient id="gradient" gradientTransform="rotate(90)">
+            <stop offset="0" stopColor="white" />
+            <stop offset="1" stopColor="black" />
+          </linearGradient>
+          <mask id="mask">
+            <rect x="0" y="0" width="2000" height="200" fill="url(#gradient)"  />
+          </mask>
+        </defs>
+      </Wave>
     </div>
   )
 }
